14401b82e66ca63243c647aa5e72f73ed8aac98d
PKM Developer <pkm@example.com>
2025-12-21 18:47:24 +0300

    Özgürlük Savaşı Trade Asistanı'nın altına taşındı - Adı güncellendi

--- pages/4_🏆_Özgürlük_Savaşı.py
+++ pages/4_🏆_Özgürlük_Savaşı.py
@@ -6,7 +6,7 @@ import pandas as pd
 import plotly.graph_objects as go
 
 st.set_page_config(
-    page_title="Challenge - Özgürlük Savaşı",
+    page_title="Özgürlük Savaşı",
     page_icon="🏆",
     layout="wide"
 )
